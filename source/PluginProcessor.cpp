e7c17d07abba9feac113e121f277d01165fdaefa
Vitalii Voronkin <viton90@gmail.com>
2023-12-28 14:16:54 -0500

    basic simple reverb is working now

--- source/PluginProcessor.cpp
+++ source/PluginProcessor.cpp
@@ -95,6 +95,14 @@ void ReverbProjectAudioProcessor::prepareToPlay(double sampleRate, int samplesPe
 {
     // Use this method as the place to do any pre-playback
     // initialisation that you need..
+
+    // rvrb.setSampleRate(sampleRate);
+    juce::dsp::ProcessSpec specs;
+    specs.sampleRate = sampleRate;
+    specs.maximumBlockSize = static_cast<juce::uint32>(samplesPerBlock);
+    specs.numChannels = static_cast<juce::uint32>(getTotalNumOutputChannels());
+
+    rvrb.prepare(specs);
 }
 
 void ReverbProjectAudioProcessor::releaseResources()
@@ -128,33 +136,53 @@ bool ReverbProjectAudioProcessor::isBusesLayoutSupported(const BusesLayout &layo
 }
 #endif
 
+void ReverbProjectAudioProcessor::updateReverbParams()
+{
+    // params.roomSize   = size->get() * 0.01f;
+    // params.damping    = damp->get() * 0.01f;
+    // params.width      = width->get() * 0.01f;
+    // params.wetLevel   = mix->get() * 0.01f;
+    // params.dryLevel   = 1.0f - mix->get() * 0.01f;
+    // params.freezeMode = freeze->get();
+
+    // rvrb.setParameters (params);
+}
+
 void ReverbProjectAudioProcessor::processBlock(juce::AudioBuffer<float> &buffer, juce::MidiBuffer &midiMessages)
 {
     juce::ScopedNoDenormals noDenormals;
     auto totalNumInputChannels = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear(i, 0, buffer.getNumSamples());
+    juce::dsp::AudioBlock<float> block(buffer);
+    juce::dsp::ProcessContextReplacing<float> ctx(block);
+    rvrb.process(ctx);
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto *channelData = buffer.getWritePointer(channel);
+    // for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+    //     buffer.clear(i, 0, buffer.getNumSamples());
 
-        // ..do something to the data...
-    }
+    // dnBuffer.resize(buffer.getNumChannels(), 0.f);
+
+    // for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    // {
+    //     auto *channelData = buffer.getWritePointer(channel);
+    //     rvrb.processStereo(&channelData[0], &channelData[1], buffer.getNumSamples());
+
+    //     for (auto i = 0; i < buffer.getNumSamples(); i++)
+    //     {
+    //         const auto inputSample = channelData[i];
+
+    //         // const auto proccessedSample = rvrb.processSample(i) * inputSample + dnBuffer[channel];
+
+    //         // dnBuffer[channel] = inputSample * proccessedSample;
+
+    //         // const auto rvrbOutput = 0.5f * inputSample * proccessedSample;
+
+    //         // channelData[i] = rvrbOutput;
+    //     }
+
+    //     // ..do something to the data...
+    // }
 }
 
 //==============================================================================
